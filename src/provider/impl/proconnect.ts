ed9c3ddc56585ecc604fa2b2c508753c88e31644
Victor Zeinstra <zeinstra.victor@gmail.com>
2026-02-19 12:18:31 +0100

    feat: ajout du provider proconnect

---
+++ src/provider/impl/proconnect.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+import { type WellKnown } from "../../utils/well-known";
+import { type Provider } from "../Provider";
+
+const base = (testServer = false): Provider => ({
+  getIssuer(pathname = "", params?: Record<string, string>): string {
+    const baseUrl = testServer
+      ? "https://app.sandbox.agentconnect.gouv.fr"
+      : "https://app.agentconnect.gouv.fr";
+    return `${baseUrl}/${pathname.replace(/^\//, "")}${params ? `?${new URLSearchParams(params).toString()}` : ""}`;
+  },
+
+  async getWellKnown(): Promise<WellKnown> {
+    const providerWellKnown = await axios.get<WellKnown>(this.getIssuer(".well-known/openid-configuration"));
+
+    return providerWellKnown.data;
+  },
+});
+
+export const proconnect = base();
+export const proconnecttest = base(true);
